c7163e4df48a5d24b2fe48042ab7bd984b26d5b7
gitmeza <nmeza4268@sdsu.edu>
2025-10-03 15:53:57 -0700

    Commit

--- main.gd
+++ main.gd
@@ -1,13 +1,15 @@
 extends Node2D
 @onready var flowerAnom = $flowerAnom
 @onready var ghostAnom = $ghostAnom
+@onready var ghostScream = $ghostScream
+@onready var flowerGiggle = $flowerGiggle
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Scene started!")
 
 	# Generate a random chance
-	if randf() < 0.1:  # 10% chance
+	if randf() < 0.5:  # 10% chance
 		_do_random_feature()
 	else:
 		print("No anomaly.")
@@ -16,3 +18,5 @@ func _do_random_feature():
 	# Spawn anomaly
 	flowerAnom.visible = true
 	ghostAnom.visible = true
+	ghostScream.playing = true
+	flowerGiggle.playing = true
